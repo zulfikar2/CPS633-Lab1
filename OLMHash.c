671fabffcac3aa8d8ac2079672c2e2e9211ad7dd
emmanuel sinlao <emmanuel sinlao>
2016-09-29 16:02:36 -0400

    Added new user ability, writing to file

--- OLMHash.c
+++ OLMHash.c
@@ -4,6 +4,7 @@
 
 #define MAX_LENGTH 32
 #define MAX_TRIES 3
+#define MAX_PASS_LENGTH 13
 
 int main() {
 	FILE* data;
@@ -12,19 +13,22 @@ int main() {
 		fprintf(stderr, "Error opening userData.txt");
 		return -1;
 	}
-	
+
 	char username[MAX_LENGTH];
 	char passHash[MAX_LENGTH];
 	char buffer[MAX_LENGTH];
 	char search[MAX_LENGTH];
+	char *ret;
 	int userLength = 1;
 	int tries = 0;
 	int found = 0;
+	int userFlag=0; // 0 for new user 1 for registered user
 	//fprintf(data, "username : password\n");
 	//printf("%s\n", passHash);    PRINT GIVEN
-	
+
 	while(userLength == 1) {
 		printf("Please give user thx\n");
+		printf("tries: %d ",tries);
 		scanf("%s", search);
 		for(userLength=0; search[userLength]!='\0'; userLength++);
 		if(userLength < 4 || userLength > MAX_LENGTH)
@@ -35,34 +39,54 @@ int main() {
 			userLength = 1;
 		}
 		else
-			userLength = 1;
+			userLength = 2; //infinite loop due to userLength always being 1 regardless of correct length
 		if(tries >= MAX_TRIES) {
-			fprintf(stderr, "Too many unsuccessful attempts - your account is locked\0");
+			fprintf(stderr, "Too many unsuccessful attempts - your account is locked\0"); //kicks you out of program
 			return -2;
 		}
 	}
-	
-	while (fgets(buffer, MAX_LENGTH, data) != NULL) {
-		if((strstr(buffer, search)) != NULL) {
-			printf("%s\n", buffer);
+
+	while (fgets(buffer, MAX_LENGTH, data) != NULL) { //read text file. read EACH LINE. while txt file isnt empty
+		if((strstr(buffer, search)) != NULL) { //search text file for username(search). if it is found
+			//printf("%s\n", buffer);
+			userFlag=1;
+			ret=strstr(buffer,search); //store line from buffer
+			//printf("Username is: %s",ret); //doesnt search so far. prints out whole file.
 			for(int x = 0; x < MAX_LENGTH; x++) {
-				if(buffer[x] == ':')
+				if(ret[x] == ':') //stop right before password
 					break;
-				username[x] = buffer[x];
+				username[x] = ret[x];
 				username[x+1] = '\0';
 			}
-			//printf("%s%s\n", username, " found in database");
-			
+			printf("%s%s\n", username, " found in database");
+		}
+
+
 			//ask for old password and then hash it and check it against database
 			//If the hashes match, the user is prompted for a new
 			//password. The hash of the new password then replaces that of
 			//the old password
 		}
-		else {
-			//add username/pass in database
-		}
-	}
-	
+		 //if user doesnt exist add username
+                if((strstr(buffer, search))== NULL && userFlag==0){ //searched username doesnt exist. but buffer still runs
+                    printf("Username doesn't exist. Username created.\n");
+                    char *pass=(char *)calloc(MAX_PASS_LENGTH, sizeof(char)); //allocate space  pass variable
+                    printf("Enter Password: "); // prompt
+                    scanf("%s",pass); // take stdin put in pass
+                    printf(" new User and Pass : %s:%s",search,pass);
+                    fprintf(data, "%s:%s\n",search,pass);
+                    //write new user and pass into database
+                    //write(data,)
+
+
+
+                }
+
+
+
+
+
+
 	fclose(data);
 	return 0;
 }
