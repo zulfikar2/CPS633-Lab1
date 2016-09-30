cfc4524ab06373db4b6b9f2adbd91c12f5adb49f
zulfikar2 <zul_king--rules@hotmail.com>
2016-09-30 17:19:33 -0400

    Added write function : has seg fault

--- OLMHash.c
+++ OLMHash.c
@@ -9,18 +9,18 @@
 
 char userData[MAX_USERS][MAX_USERNAME_LENGTH];
 char passData[MAX_USERS][MAX_PASS_LENGTH];
+int users = 0;
 
 void E(char *in, char *out);
 int initData();
 char* getUser();
-char* getPass(int passFlag);
+char* getPass(int);
 int cmprUser(char*);
 int cmprPass(char*, int);
-char* hashIt(char* pass, int length);
-char* stripper(char*);
+char* hashIt(char*, int);
+int writeData();
 
 int main() {
-	
 	//initialize userData and passData arrays.
 	initData();
 
@@ -57,15 +57,44 @@ int main() {
 	else if (realUser == -1) {
 		//User added to database, enter password
 		password = getPass(2);
+		//printf("copy\n");
+		strncpy(userData[users], username, strlen(username));
+		//printf("copy2\n");
+		strncpy(passData[users], password, strlen(password));
+		//printf("copy3\n");
+		users++;
+		//printf("gunn write soon\n");
+		writeData();
 	}
 	return 0;
 }
 
+int writeData() {
+	printf("opening file\n");
+	FILE* data = fopen("userData.txt", "w+");
+	if(!data) {
+		fprintf(stderr, "Error creating userData.txt to write\n");
+		return -1;
+	}
+	
+	int userCount = 0;
+	printf("ABOUT TO WRITE");
+	while(userCount <= users) {
+		char* towrite;
+		strcat(towrite, userData[userCount]);
+		strcat(towrite, ":");
+		strcat(towrite, passData[userCount]);
+		fwrite(towrite, 1, sizeof(towrite), data);
+		printf("Wrote %s", towrite);
+		userCount++;
+	}
+	return 1;
+}
 
 int initData() {
 	FILE* data = fopen("userData.txt", "r+");
 	if(!data) {
-		fprintf(stderr, "Error opening userData.txt");
+		fprintf(stderr, "Error opening userData.txt to read or no database found\n");
 		return -1;
 	}
 
@@ -80,24 +109,28 @@ int initData() {
 		token = strtok(buffer, splitChar);
 		while(token != NULL) {
 			if(counter%2 == 0) {
-				strncpy(userData[counter/2], token, 12);
+				strncpy(userData[counter/2], token, MAX_USERNAME_LENGTH);
 				printf("%s Added User\n", userData[counter/2]);
+				users++;
 			}
 			else {
 				sscanf(token, "%12s", token);
-				strncpy(passData[counter/2], token, 12);
+				strncpy(passData[counter/2], token, MAX_PASS_LENGTH);
 				printf("%s Added Pass\n", passData[counter/2]);
 			}
 			token = strtok(NULL, splitChar);
 			counter++;
 		}
 	}
+	
+	fclose(data);
+	return 1;
 }
 
 int cmprUser(char* check) {
 	//search for stdin username to see if it matches any in array. return 1 if found. return -1
-	for(int i =0; i < MAX_USERS ; i++){
-		printf("Searching for matches for %s with %s\n", check, userData[i]);
+	for(int i =0; i < users ; i++){
+		//printf("Searching for matches for %s with %s\n", check, userData[i]);
 		if(strcmp(check,userData[i]) == 0){
 			printf("Match found!\n");
 			return i;
@@ -107,17 +140,17 @@ int cmprUser(char* check) {
 	return -1;
 }
 int cmprPass(char* check, int index) {
-	printf("Searching for matched for %s with %s\n", check, passData[index]);
+	//printf("Searching for matched for %s with %s\n", check, passData[index]);
 	if(strcmp(check,passData[index]) == 0){
-		printf("Match found!\n");
+		//printf("Match found!\n");
 		return index;
 	}
-	printf("Match not found!\n");
+	//printf("Match not found!\n");
 	return -1;
 }
 
 char* getUser() {
-	printf("GET USER\n");
+	//printf("GET USER\n");
 	char* username = (char*)calloc(MAX_USERNAME_LENGTH, sizeof(char));
 	int userLength = 0;
 	printf("Please give user thx\n");
