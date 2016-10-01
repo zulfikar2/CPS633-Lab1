af0390a20693ffb01fb52af6d88333fc9d1e6580
josh245 <joshcolaco@hotmail.com>
2016-10-01 12:01:17 -0400

    frikken kids
    frikken kids

---
+++ CRA.c
@@ -0,0 +1,214 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_PASS_LENGTH 13
+#define MAX_USERNAME_LENGTH 33
+#define MAX_USERS 50
+
+char userData[MAX_USERS][MAX_USERNAME_LENGTH];
+char passData[MAX_USERS][MAX_PASS_LENGTH];
+int users = 0;
+
+
+void existingUser(char* username, int index);
+char* randomXOR(char* hash, int r);
+void XorIt(char* in, char* out, int r);
+int writeData();
+int initData();
+char* getUser();
+int cmprUser(char* check);
+void newUser(char* username);
+char* getPass(int passFlag);
+void E(char *in, char *out);
+char* hashIt(char* pass, int length);
+
+int main()
+{
+	initData();
+	srand(time(NULL));
+	char* username = 0;
+	//Asks for userinput from user.
+	while(username == 0)
+	{
+		username = getUser();
+	}
+	
+	int realUser = cmprUser(username);
+	if(realUser == -1)
+	{
+		newUser(username);
+	}
+	else
+	{
+		existingUser(username, realUser);
+	}
+}
+
+int writeData() {
+	FILE* data = fopen("userData.txt", "w+");
+
+	if(!data) {
+		fprintf(stderr, "Error opening userData.txt to append\n");
+		return -1;
+	}
+	for(int i = 0; i < users; i++)
+		fprintf(data, "%s:%s\n", userData[i], passData[i]);
+	fclose(data);
+	return 1;
+}
+
+int initData() {
+	FILE* data = fopen("userData.txt", "r+");
+	if(!data) {
+		fprintf(stderr, "Error opening userData.txt to read or no database found\n");
+		return -1;
+	}
+
+	const size_t BUFFER = 255;
+	char* buffer = (char*)calloc(BUFFER, sizeof(char));
+	const char splitChar[2] = ":";
+	int counter = 0;
+	char* token;
+	int length;
+	
+	while(fgets(buffer, BUFFER, data) != NULL) {
+		token = strtok(buffer, splitChar);
+		while(token != NULL) {
+			if(counter%2 == 0) {
+				strncpy(userData[counter/2], token, MAX_USERNAME_LENGTH);
+				printf("%s Added User\n", userData[counter/2]);
+				users++;
+			}
+			else {
+				sscanf(token, "%12s", token);
+				strncpy(passData[counter/2], token, MAX_PASS_LENGTH);
+				printf("%s Added Pass\n", passData[counter/2]);
+			}
+			token = strtok(NULL, splitChar);
+			counter++;
+		}
+	}
+	
+	fclose(data);
+	return 1;
+}
+
+char* getUser() {
+	//printf("GET USER\n");
+	char* username = (char*)calloc(MAX_USERNAME_LENGTH, sizeof(char));
+	int userLength = 0;
+	printf("Please give user thx\n");
+	scanf("%s", username);
+	
+	userLength = strlen(username);
+
+	if(userLength < 4 || userLength > MAX_USERNAME_LENGTH) {
+		fprintf(stderr,"Invalid username length\n");
+		free(username);
+		return "-1";
+	}
+	
+	return username;
+}
+
+int cmprUser(char* check) {
+	//search for stdin username to see if it matches any in array. return 1 if found. return -1
+	for(int i =0; i < users ; i++){
+		//printf("Searching for matches for %s with %s\n", check, userData[i]);
+		if(strcmp(check,userData[i]) == 0){
+			printf("Match found!\n");
+			return i;
+		}
+	}
+	printf("Match not found!\n");
+	return -1;
+}
+
+char* getPass(int passFlag){
+    char* password= (char*)calloc(MAX_PASS_LENGTH,sizeof(char));
+    int passlength = 0;
+	if(passFlag == 1)
+		printf("User was found in database, enter old password:\n");
+	else if (passFlag == 2)
+		printf("User added to database, enter password:\n");
+	else if (passFlag == 3)
+		printf("Passwords match, enter new password for username:\n");
+	scanf("%s", password);
+	passlength = strlen(password);
+	if(passlength > MAX_PASS_LENGTH) {
+		for(int passlength; passlength > MAX_PASS_LENGTH; passlength--)
+		{
+			password[passlength] = '\0';
+		}
+		return password;
+	}
+	return password;
+}
+
+char* hashIt(char* pass, int length) {
+	char* hashed = calloc(MAX_PASS_LENGTH,sizeof(char));
+	for(int i = 0; pass[i] != '\0'; i++)
+		E(&pass[4*i], &hashed[4*i]);
+	return hashed;
+}
+
+void E(char *in, char *out)
+{
+out[0]=(in[0]&0x80)^(((in[0]>>1)&0x7F)^((in[0])&0x7F));
+out[1]=((in[1]&0x80)^((in[0]<<7)&0x80))^(((in[1]>>1)&0x7F)^((in[1])&0x7F));
+out[2]=((in[2]&0x80)^((in[1]<<7)&0x80))^(((in[2]>>1)&0x7F)^((in[2])&0x7F));
+out[3]=((in[3]&0x80)^((in[2]<<7)&0x80))^(((in[3]>>1)&0x7F)^((in[3])&0x7F));
+}
+
+void newUser(char* username)
+{
+	printf("Enter new user's password:\n");
+	char* pass = getPass(2);
+	pass = hashIt(pass, strlen(pass));
+	strncpy(userData[users], username, strlen(username));
+	strncpy(passData[users], pass, strlen(pass));
+	users++;
+	writeData();
+	printf("User added.\n");
+}
+
+void existingUser(char* username, int index)
+{
+	char* pass = getPass(1);
+	pass = hashIt(pass, strlen(pass));
+	
+	int r = rand();
+	char* client = randomXOR(pass, r);
+	char* server = randomXOR(passData[index], r);
+	
+	if(strcmp(client, server) == 0)
+	{
+		printf("Access granted!\n");
+	}
+	else
+	{
+		printf("Access denied!\n");	
+	}
+}
+
+char* randomXOR(char* pass, int r)
+{
+	char * out = (char*)calloc(MAX_PASS_LENGTH, sizeof(char));
+	XorIt(pass, out, r);
+	XorIt(&pass[4], &out[4], r);
+	XorIt(&pass[8], &out[8], r);
+	return out;	
+}
+
+void XorIt(char* in, char* out, int r)
+{
+	out[3] = in[3] ^ (r & 255);
+	r = r >> 8; 
+	out[2] = in[2] ^ (r & 255);
+	r = r >> 8;
+	out[1] = in[1] ^ (r & 255);
+	r = r >> 8;
+	out[0] = in[0] ^ (r & 255);
+}
