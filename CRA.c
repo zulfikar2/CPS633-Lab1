77220089946759a18c24ae97d162d4ccba6c31cc
josh245 <joshcolaco@hotmail.com>
2016-10-01 13:02:07 -0400

    Update CRA.c

--- CRA.c
+++ CRA.c
@@ -177,8 +177,7 @@ void newUser(char* username)
 void existingUser(char* username, int index)
 {
 	char* pass = getPass(1);
-	pass = hashIt(pass, strlen(pass));
-	
+	pass = hashIt(pass, strlen(pass));	
 	int r = rand();
 	char* client = randomXOR(pass, r);
 	char* server = randomXOR(passData[index], r);
@@ -193,15 +192,6 @@ void existingUser(char* username, int index)
 	}
 }
 
-char* randomXOR(char* pass, int r)
-{
-	char * out = (char*)calloc(MAX_PASS_LENGTH, sizeof(char));
-	XorIt(pass, out, r);
-	XorIt(&pass[4], &out[4], r);
-	XorIt(&pass[8], &out[8], r);
-	return out;	
-}
-
 void XorIt(char* in, char* out, int r)
 {
 	out[3] = in[3] ^ (r & 255);
@@ -211,4 +201,13 @@ void XorIt(char* in, char* out, int r)
 	out[1] = in[1] ^ (r & 255);
 	r = r >> 8;
 	out[0] = in[0] ^ (r & 255);
+}
+
+char* randomXOR(char* pass, int r)
+{
+	char * out = (char*)calloc(MAX_PASS_LENGTH, sizeof(char));
+	XorIt(pass, out, r);
+	XorIt(&pass[4], &out[4], r);
+	XorIt(&pass[8], &out[8], r);
+	return out;	
 }
